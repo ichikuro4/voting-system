acf8edd232cece6d47744b33774ae0402dd5e492
Ichikuro <jerson_anthon@hotmail.com>
2026-05-02 09:27:50 -0500

    Se actualizo navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -45,7 +45,7 @@ export default function RootLayout({ children }) {
                   href="/admin/login"
                   className="rounded-full px-4 py-2 transition hover:bg-slate-100 hover:text-slate-950"
                 >
-                  Resultados (Admin)
+                  Resultados
                 </Link>
               </nav>
             </div>
